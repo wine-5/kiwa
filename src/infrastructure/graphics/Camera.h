8a2b0f93f1c68c9ca6799d385a9465931c9d8f23
YutoImata <yuto.imata.wine@gmail.com>
2026-09-17 22:17:20 +0900

    feat: カメラの処理を追加

---
+++ src/infrastructure/graphics/Camera.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include "core/base/NonCopyable.h"
+#include "core/interface/ICamera.h"
+
+namespace infrastructure::graphics
+{
+	/**
+	 * @brief DxLib のカメラを扱う ICamera の実装
+	 */
+	class Camera final : public core::iface::ICamera, private core::base::NonCopyable
+	{
+	  public:
+		Camera() = default;
+
+		void lookAt(const core::utility::Vector3& position, const core::utility::Vector3& target) override;
+
+		void setPerspective(float fovRadian, float nearZ, float farZ) override;
+
+		[[nodiscard]] core::utility::Vector3 getPosition() const noexcept override
+		{
+			return m_position;
+		}
+
+	  private:
+		core::utility::Vector3 m_position{};
+	};
+} // namespace infrastructure::graphics
