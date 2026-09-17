0f2aedf0ee31412fa2ef9bad49dd8010dc4b66b6
YutoImata <yuto.imata.wine@gmail.com>
2026-09-17 20:01:33 +0900

    feat: シーンを生成するファクトリを追加

---
+++ src/game/scene/SceneFactory.cpp
@@ -0,0 +1,21 @@
+﻿#include "game/scene/SceneFactory.h"
+#include "game/scene/InGameScene.h"
+#include "game/scene/ResultScene.h"
+#include "game/scene/TitleScene.h"
+
+namespace game::scene
+{
+	std::unique_ptr<IScene> SceneFactory::create(SceneType sceneType, const SceneContext& context)
+	{
+		switch (sceneType)
+		{
+		case SceneType::InGame:
+			return std::make_unique<InGameScene>(context);
+		case SceneType::Result:
+			return std::make_unique<ResultScene>(context);
+		case SceneType::Title:
+		default:
+			return std::make_unique<TitleScene>(context);
+		}
+	}
+} // namespace game::scene
---
+++ src/game/scene/SceneFactory.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include "game/scene/SceneType.h"
+#include <memory>
+
+namespace game::scene
+{
+	class IScene;        // 前方宣言
+	struct SceneContext; // 前方宣言
+
+	/**
+	 * @brief シーンの種類から実体を作るファクトリ
+	 *
+	 * 「どの種類がどのクラスか」を知っているのはここだけにして、
+	 * SceneManager が個々のシーンを include せずに済むようにしている
+	 */
+	class SceneFactory
+	{
+	  public:
+		/**
+		 * @brief シーンを生成する
+		 * @param sceneType 生成するシーンの種類
+		 * @param context シーンへ渡す外部機能の束
+		 * @return 生成したシーン
+		 */
+		[[nodiscard]] static std::unique_ptr<IScene> create(SceneType sceneType, const SceneContext& context);
+	};
+} // namespace game::scene
