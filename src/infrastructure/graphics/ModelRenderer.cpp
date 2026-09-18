8c754ed46568d9e57f449053d90f8cf606680cd2
YutoImata <yuto.imata.wine@gmail.com>
2026-09-18 23:14:00 +0900

    fix: モデル描画の奥行き判定を有効にする

--- src/infrastructure/graphics/ModelRenderer.cpp
+++ src/infrastructure/graphics/ModelRenderer.cpp
@@ -18,6 +18,11 @@ namespace infrastructure::graphics
 		if (handle < 0)
 			return -1;
 
+		// モデルの描画は既定で奥行きを見ない。器のように内と外の面が重なる形は、
+		// 有効にしておかないと後から描いた面が手前の面を上書きしてしまう
+		MV1SetUseZBuffer(handle, TRUE);
+		MV1SetWriteZBuffer(handle, TRUE);
+
 		m_models.emplace(path, handle);
 		return handle;
 	}
@@ -32,9 +37,17 @@ namespace infrastructure::graphics
 		MV1SetRotationXYZ(handle, VGet(rotationRadian.x, rotationRadian.y, rotationRadian.z));
 		MV1SetPosition(handle, VGet(position.x, position.y, position.z));
 
-		// モデルは自前で陰影を焼いていないので、描く間だけ光を当てる
+		// モデルは自前で陰影を焼いていないので、描く間だけ光を当てる。
+		// 奥行きの判定も既定では切れているため、ここで入れる（切れていると
+		// 後から描いたものが器の手前に出てしまう）
 		SetUseLighting(TRUE);
+		SetUseZBufferFlag(TRUE);
+		SetWriteZBufferFlag(TRUE);
+
 		MV1DrawModel(handle);
+
+		SetWriteZBufferFlag(FALSE);
+		SetUseZBufferFlag(FALSE);
 		SetUseLighting(FALSE);
 	}
 } // namespace infrastructure::graphics
