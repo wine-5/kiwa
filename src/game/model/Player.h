556f6a466e6b1e0dba1f14005aaf81c5bcf29a3d
YutoImata <yuto.imata.wine@gmail.com>
2026-09-20 18:25:11 +0900

    feat: 対局する２人のプレイヤーを定義

---
+++ src/game/model/Player.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+namespace game::model
+{
+	/**
+	 * @brief 対局する二人
+	 */
+	enum class Player
+	{
+		One, // 一の手
+		Two, // 二の手
+	};
+
+	/**
+	 * @brief 相手を返す
+	 * @param player 一方
+	 * @return もう一方
+	 */
+	[[nodiscard]] constexpr Player returnOpponent(Player player) noexcept
+	{
+		return player == Player::One ? Player::Two : Player::One;
+	}
+} // namespace game::model
